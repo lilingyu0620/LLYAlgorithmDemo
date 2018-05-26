6422d82e0d3252e4c955bdc244f52dcb4b617904
lly <490859662@qq.com>
2018-05-26 16:48:31 +0800

    kmp

--- LLYAlgorithmDemo/LLYAlgorithmDemo/algorithm/KMP.m
+++ LLYAlgorithmDemo/LLYAlgorithmDemo/algorithm/KMP.m
@@ -17,22 +17,102 @@
     self = [super init];
 
     if (self) {
-        [self kmpArrayWithNeedleStr:@"aabbbbabdbdbc"];
+        
+        NSString *oriStr = @"aabbababbaaba";
+        NSString *needleStr = @"bbaaba";
+        
+        int index = [self forceIndex:oriStr needleStr:needleStr];
+        NSLog(@"index = %d",index);
+        
+        [self kmpArrayWithNeedleStr:needleStr];
+        int indexKMP = [self indexKMP:oriStr needleStr:needleStr];
+        NSLog(@"index = %d",indexKMP);
     }
     
     return self;
 }
 
+//暴力匹配
+- (int)forceIndex:(NSString *)oriStr needleStr:(NSString *)needleStr{
+    if (needleStr.length <= 0) {
+        return 0;
+    }
+    
+    if (oriStr.length <= 0) {
+        return  -1;
+    }
+    
+    int i = 0,j = 0;
+    while (i < oriStr.length) {
+        if ([oriStr characterAtIndex:i] == [needleStr characterAtIndex:j]) {
+            i++;
+            j++;
+        }else{
+            i = i - j + 1;
+            j = 0;
+        }
+        
+        if (j == needleStr.length) {
+            return i - j;
+        }
+    }
+    return -1;
+}
+
+- (int)indexKMP:(NSString *)oriStr needleStr:(NSString *)needleStr{
+    
+    if (needleStr.length <= 0) {
+        return 0;
+    }
+    
+    if (oriStr.length <= 0) {
+        return  -1;
+    }
+    
+    int i = 0,j = 0;
+    while (i < oriStr.length && j < needleStr.length) {
+        if (j == 0 || [oriStr characterAtIndex:i] == [needleStr characterAtIndex:j]) {
+            i++;
+            j++;
+        }
+        else{
+            j = [_kmpArray[j] intValue];
+        }
+    }
+    
+    if (j == needleStr.length) {
+        return i - j;
+    }
+    else{
+        return -1;
+    }
+    
+}
 
 - (void)kmpArrayWithNeedleStr:(NSString *)needleStr{
     
     _kmpArray = [NSMutableArray arrayWithCapacity:(needleStr.length - 1)];
-    for (int i = 0; i < needleStr.length - 1; i++) {
+    for (int i = 0; i < needleStr.length; i++) {
         [_kmpArray addObject:@0];
     }
     
     NSLog(@"_kmpArray = %@",_kmpArray);
     
+    int i = 0,j = 1;
+    while (j < needleStr.length - 1) {
+        
+        if (i == 0 || [needleStr characterAtIndex:i] == [needleStr characterAtIndex:j]) {
+            ++i;
+            ++j;
+            _kmpArray[j] = @(i);
+        }
+        else{
+            i = [_kmpArray[i] intValue];
+        }
+    }
+    
+    NSLog(@"_kmpArray = %@",_kmpArray);
+
 }
 
 
